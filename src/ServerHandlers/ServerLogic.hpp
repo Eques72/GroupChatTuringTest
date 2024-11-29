395530baffcac568432eda5f17524f9fe90d497d
eccentricfae <eccentricfae@gmail.com>
2024-11-29 19:27:28 +0100

    Deleted default ServerLogic constructors

--- src/ServerHandlers/ServerLogic.hpp
+++ src/ServerHandlers/ServerLogic.hpp
@@ -14,7 +14,15 @@ class ServerLogic
 {
 public:
 
-    ServerLogic(uWS::Loop * const p_loop);
+    explicit ServerLogic(uWS::Loop * const p_loop);
+
+    ServerLogic() = delete;
+    ServerLogic(ServerLogic const &) = delete;
+    ServerLogic(ServerLogic &&) = delete;
+    ServerLogic& operator=(ServerLogic const &) = delete;
+    ServerLogic& operator=(ServerLogic &&) = delete;
+
+    ~ServerLogic() = default;
 
     void connection_established_handler(uWS::WebSocket<SSL, true, PerSocketData> * ws);
     void message_handler(uWS::WebSocket<SSL, true, PerSocketData> * ws, std::string_view msg, uWS::OpCode opCode);
