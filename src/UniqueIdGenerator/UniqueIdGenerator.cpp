6e97371be7634d6cbb93f498cabf4b9fbf2d80f6
eccentricfae <eccentricfae@proton.me>
2025-01-14 23:24:46 +0100

    Shorten IDs to 4 digits or less

--- src/UniqueIdGenerator/UniqueIdGenerator.cpp
+++ src/UniqueIdGenerator/UniqueIdGenerator.cpp
@@ -7,7 +7,8 @@ auto UniqueIdGenerator::generate_random_unique_id() -> int32_t
 {
     static std::random_device rd;
     static std::mt19937 gen(rd());
-    static std::uniform_int_distribution<int32_t> dis(0, std::numeric_limits<int32_t>::max());
+    // static std::uniform_int_distribution<int32_t> dis(0, std::numeric_limits<int32_t>::max());
+    static std::uniform_int_distribution<int32_t> dis(0, 9999);
 
     std::unique_lock lock{m_mutex, std::defer_lock};
 
